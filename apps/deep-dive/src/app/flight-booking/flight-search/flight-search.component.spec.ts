83e7ec7316e6bc649560ebd7c466bcba24023013
mikezks <23002661+mikezks@users.noreply.github.com>
2022-11-25 11:42:46 +0100

    feat: component testing with mocks

--- apps/deep-dive/src/app/flight-booking/flight-search/flight-search.component.spec.ts
+++ apps/deep-dive/src/app/flight-booking/flight-search/flight-search.component.spec.ts
@@ -11,20 +11,63 @@ import { FlightSearchComponent } from './flight-search.component';
 describe('Unit test: flight-search.component', () => {
   let component: FlightSearchComponent;
   let fixture: ComponentFixture<FlightSearchComponent>;
+  const result = [
+    { id: 17, from: 'Graz', to: 'Hamburg', date: 'now', delayed: true },
+    { id: 18, from: 'Vienna', to: 'Barcelona', date: 'now', delayed: true },
+    { id: 19, from: 'Frankfurt', to: 'Salzburg', date: 'now', delayed: true },
+  ];
+
+  const flightServiceMock = {
+    find(): Observable<Flight[]> {
+      return of(result);
+    },
+    // Implement the following members only if this code is used in your Component
+    flights: [],
+    load(): void {
+      this.find().subscribe(f => { (this.flights as Flight[]) = f; });
+    }
+  };
+
+  // eslint-disable-next-line @angular-eslint/component-selector
+  @Component({ selector: 'flight-card', template: '' })
+  class FlightCardComponent {
+    @Input() item: Flight | undefined;
+    @Input() selected = false;
+    @Output() selectedChange = new EventEmitter<boolean>();
+  }
+
+  // eslint-disable-next-line @angular-eslint/directive-selector
+  @Directive({ selector: 'input[city]' })
+  class CityValidatorDirective {
+    @Input() city: string[] = [];
+    validate = () => null;
+  }
+
+  @Pipe({ name: 'city' })
+  class CityPipe implements PipeTransform {
+    transform = (v: unknown) => v;
+  }
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-
+        FormsModule
       ],
       declarations: [
-
+        FlightSearchComponent,
+        FlightCardComponent,
+        CityPipe,
+        CityValidatorDirective
       ]
     })
       .overrideComponent(FlightSearchComponent, {
         set: {
           providers: [
-
+            {
+              provide: FlightService,
+              useValue: flightServiceMock
+            }
           ]
         }
       })
@@ -35,9 +78,11 @@ describe('Unit test: flight-search.component', () => {
     fixture.detectChanges();
   });
 
-
   it('should load flights w/ from and to', () => {
+    component.from = 'Hamburg';
+    component.to = 'Graz';
+    component.search();
 
-      expect();
+    expect(component.flights.length).toBeGreaterThan(0);
   });
 });
